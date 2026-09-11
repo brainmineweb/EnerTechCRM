538bcca484fcf904d6da3cd75512283d960b5f33
Admin <frappe@Enertech.New>
2026-09-11 12:09:33 +0000

    fixed Opportunity sales person name adding in followup activity

--- enertechv1/public/js/Opportunity.js
+++ enertechv1/public/js/Opportunity.js
@@ -1,15 +1,29 @@
 frappe.ui.form.on('Opportunity', {
     refresh: function(frm) {
-        if (!frm.is_new()) {
-            frm.add_custom_button('Create Opportunity', function() {
-                frm.copy_doc(function(new_doc) {
-                    // this callback fires on the new doc, before routing
-                    new_doc.status = 'Open';
-                    new_doc.opportunity_owner = frappe.session.user;
-                    new_doc.transaction_date = frappe.datetime.get_today();
-                    new_doc.docstatus = 0;
-                });
-            });
-        }
+        set_contact_default(frm);
     }
 });
+
+function set_contact_default(frm) {
+    if (frm.doc.opportunity_from !== 'Lead' || !frm.doc.party_name) return;
+
+    frappe.db.get_list('Contact', {
+        filters: [
+            ['Dynamic Link', 'link_doctype', '=', 'Lead'],
+            ['Dynamic Link', 'link_name', '=', frm.doc.party_name]
+        ],
+        fields: ['name'],
+        order_by: '`tabContact`.creation asc',
+        limit: 1
+    }).then(contacts => {
+        if (!contacts.length) return;
+
+        let meta = frappe.get_meta('Follow Up Activities');
+        let field = meta.fields.find(f => f.fieldname === 'contact_with');
+        if (field) {
+            field.default = contacts[0].name;
+        }
+    }).catch(err => {
+        console.error('Contact fetch failed:', err);
+    });
+}
